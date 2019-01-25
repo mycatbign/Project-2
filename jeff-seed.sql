5e3a38c55db7689dfa84fc7c944c6f935adae8a9
Jeff Bond <jbond2663@yahoo.com>
2019-01-25 00:46:25 -0500

    updated .env with all JawsDB variables

---
+++ jeff-seed.sql
@@ -0,0 +1,16 @@
+use user;
+
+
+insert into hikers(user, password,firstName,lastName,displayName,information,imagetext,createdAt,updatedAt)
+values ("Jeffery", "Jeffery","Jeffery", "Jeffery","Jeffery", "Jeffery","Jeffery",now(),now());
+
+insert into hikers(user, password,firstName,lastName,displayName,information,imagetext,createdAt,updatedAt)
+values ("Anyalynn", "Anyalynn","Anyalynn", "Anyalynn","Anyalynn", "Anyalynn","Anyalynn",now(),now());
+
+insert into hikers(user, password,firstName,lastName,displayName,information,imagetext,createdAt,updatedAt)
+values ("Theresa", "Theresa","Theresa", "Theresa","Theresa", "Theresa","Theresa",now(),now());
+
+insert into hikers(user, password,firstName,lastName,displayName,information,imagetext,createdAt,updatedAt)
+values ("Jackson", "Jackson","Jackson", "Bond","Jackson-the-Hiker", "A dog who loves the mountains!","Jeffery",now(),now());
+
+
